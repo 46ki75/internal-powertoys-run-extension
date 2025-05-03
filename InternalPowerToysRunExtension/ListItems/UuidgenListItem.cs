1f645ce910395503d1cfbfbfd507538fa45d873c
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-03 11:43:05 +0900

    feat: implement uuid generation command

---
+++ InternalPowerToysRunExtension/ListItems/UuidgenListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace InternalPowerToysRunExtension.ListItems
+{
+    internal sealed partial class UuidgenListItem : ListItem
+    {       
+        public UuidgenListItem() :
+            base(new NoOpCommand())
+        {
+            Command = new AnonymousCommand(action: Generate) { Result = CommandResult.KeepOpen() };
+            Title = "Increment";
+        }
+
+        private void Generate()
+        {
+            Subtitle = Guid.NewGuid().ToString();
+        }
+    }
+}
--- InternalPowerToysRunExtension/Pages/InternalPowerToysRunExtensionPage.cs
+++ InternalPowerToysRunExtension/Pages/InternalPowerToysRunExtensionPage.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using InternalPowerToysRunExtension.ListItems;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
@@ -19,7 +20,8 @@ internal sealed partial class InternalPowerToysRunExtensionPage : ListPage
     public override IListItem[] GetItems()
     {
         return [
-            new ListItem(new NoOpCommand()) { Title = "TODO: Implement your extension here" }
+            new ListItem(new NoOpCommand()) { Title = "TODO: Implement your extension here" },
+            new UuidgenListItem(),
         ];
     }
 }
